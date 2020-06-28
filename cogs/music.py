5935ac82ee8fd0e3cadd6fe6bf202b1161184a2d
MrSpaar <62711036+MrSpaar@users.noreply.github.com>
2020-06-29 00:23:13 +0200

    Music related commands
    Ability to play music on multiple servers at the same time

--- cogs/music.py
+++ cogs/music.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 from discord.utils import get
 
 import youtube_dl
-import requests
 import asyncio
 
 
@@ -23,9 +22,6 @@ class Music(commands.Cog):
     @staticmethod
     def search(author, arg):
         ydl_opts = {'format': 'bestaudio', 'noplaylist':'True'}
-        try: requests.get("".join(arg))
-        except: arg = " ".join(arg)
-        else: arg = "".join(arg)
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(f"ytsearch:{arg}", download=False)['entries'][0]
             
@@ -39,31 +35,30 @@ class Music(commands.Cog):
         return {'embed': embed, 'source': info['formats'][0]['url'], 'title': info['title']}
 
     async def display_message(self, ctx):
-        embed = self.song_queue[0]['embed']
-        content = "\n".join([f"({self.song_queue.index(i)}) {i['title']}" for i in self.song_queue[1:]]) if len(self.song_queue) > 1 else "No music in queue"
-        embed.set_field_at(index=3, name="File d'attente :", value=content, inline=False)
-        await self.message.edit(embed=embed)
+        embed = self.song_queue[ctx.guild][0]['embed']
+        content = "\n".join([f"({self.song_queue[ctx.guild].index(i)}) {i['title']}" for i in self.song_queue[ctx.guild][1:]]) if len(self.song_queue[ctx.guild]) > 1 else "No songs queued"
+        embed.set_field_at(index=3, name="Queue:", value=content, inline=False)
+        await self.message[ctx.guild].edit(embed=embed)
 
     def play_next(self, ctx):
         voice = get(self.bot.voice_clients, guild=ctx.guild)
-        if len(self.song_queue) > 1:
-            del self.song_queue[0]
+        if len(self.song_queue[ctx.guild]) > 1:
+            del self.song_queue[ctx.guild][0]
             asyncio.run_coroutine_threadsafe(self.display_message(ctx), self.bot.loop)
-            voice.play(discord.FFmpegPCMAudio(self.song_queue[0]['source'], **self.FFMPEG_OPTIONS), after=lambda e: self.play_next(ctx))
+            voice.play(discord.FFmpegPCMAudio(self.song_queue[ctx.guild][0]['source'], **self.FFMPEG_OPTIONS), after=lambda e: self.play_next(ctx))
             voice.is_playing()
         else:
             asyncio.run_coroutine_threadsafe(voice.disconnect(), self.bot.loop)
-            asyncio.run_coroutine_threadsafe(self.message.delete(), self.bot.loop)
+            asyncio.run_coroutine_threadsafe(self.message[ctx.guild].delete(), self.bot.loop)
 
     @commands.command(aliases=['p'], brief='!play [url/key-words]', description='Plays youtube videos')
-    async def play(self, ctx, *arg):
+    async def play(self, ctx, *, arg):
         channel = ctx.message.author.voice.channel
         await ctx.channel.purge(limit=1)
 
         if channel:
             voice = get(self.bot.voice_clients, guild=ctx.guild)
             song = self.search(ctx.author.mention, arg)
-            self.song_queue.append(song)
 
             if voice and voice.is_connected():
                 await voice.move_to(channel)
@@ -71,10 +66,12 @@ class Music(commands.Cog):
                 voice = await channel.connect()
 
             if not voice.is_playing():
-                self.message = await ctx.send(embed=song['embed'])
+                self.song_queue[ctx.guild] = [song]
+                self.message[ctx.guild] = await ctx.send(embed=song['embed'])
                 voice.play(discord.FFmpegPCMAudio(song['source'], **self.FFMPEG_OPTIONS), after=lambda e: self.play_next(ctx))
                 voice.is_playing()
             else:
+                self.song_queue[ctx.guild].append(song)
                 await self.display_message(ctx)
         else:
             await ctx.send("❌ Tu n'es connecté à aucun channel !", delete_after = 5.0)
@@ -106,10 +103,11 @@ class Music(commands.Cog):
             await ctx.send("❌ I'm not playing anything!", delete_after = 5.0)
 
    @commands.command(brief='!remove [vidéo]', description="Removes the video from the queue")
-    async def remove(self, ctx, arg):
-        for video in self.song_queue:
+    async def remove(self, ctx, *, arg):
+        await ctx.channel.purge(limit=1)
+        for video in self.song_queue[ctx.guild][1:]:
             if arg.lower() in video['title'].lower():
-                self.song_queue.remove(video)
+                self.song_queue[ctx.guild].remove(video)
         await self.display_message(ctx)
 
 def setup(bot):
