0ff1706dd7eb30c7a222083927ee444f429f1895
MrSpaar <62711036+MrSpaar@users.noreply.github.com>
2020-07-02 21:47:34 +0200

    Chat related commands

--- cogs/chat.py
+++ cogs/chat.py
@@ -6,28 +6,25 @@ class chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(brief='!clear [x]', description='Deletes the [x] previous messages', aliases=['delete', 'purge'])
+    @commands.command(brief='!clear [x]', aliases=['delete', 'purge'])
     async def clear(self, ctx, limit):
         try:
             float(limit)
         except:
             await ctx.send('❌ Input must be an integer!')
         else:
-            if str(ctx.channel) == 'bot':
-                limit = int(limit) + 1
-                await ctx.channel.purge(limit=limit)
-            else:
-                await ctx.send('❌ This command can only be used in "bot" channel!')
+            limit = int(limit) + 1
+            await ctx.channel.purge(limit=limit)
     
-    @commands.command(brief='!help', description="Displays the help message")
+    @commands.command(brief='!help')
     async def help(self, ctx):
         await ctx.channel.purge(limit=1)
-        embed = discord.Embed(color=discord.Color.blue(), title='Listes des commandes')
+        embed = discord.Embed(color=discord.Color.blue(), title='Commands list:')
         for cog in self.bot.cogs:
             temp = []
             for cmd in self.bot.get_cog(cog).get_commands():
                 if not cmd.hidden:
-                    temp.append(f"{cmd.brief}: {cmd.description}\n")
+                    temp.append(f"{cmd.brief}\n")
             embed.add_field(name=f'**{cog} :**', value=f"{''.join(temp)}", inline=False)
         await ctx.send(embed=embed)
 
