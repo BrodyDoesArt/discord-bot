3b11e212262a1145525ea00f2e040a980bc0efc4
MrSpaar <62711036+MrSpaar@users.noreply.github.com>
2020-07-02 21:08:10 +0200

    Chat related commands
    Optimized the help command

--- cogs/chat.py
+++ cogs/chat.py
@@ -19,32 +19,17 @@ class chat(commands.Cog):
             else:
                 await ctx.send('❌ This command can only be used in "bot" channel!')
     
-    @commands.command(brief='!help [category/nothing]', description="Displays the help message")
-    async def help(self, ctx, *c):
-        if not c:
-            await ctx.channel.purge(limit=1)
+    @commands.command(brief='!help', description="Displays the help message")
+    async def help(self, ctx):
+        await ctx.channel.purge(limit=1)
+        embed = discord.Embed(color=discord.Color.blue(), title='Listes des commandes')
+        for cog in self.bot.cogs:
             temp = []
-            embed = discord.Embed(color=discord.Color.blue(), title='Command list:')
-            for x in self.bot.cogs:
-                for y in self.bot.get_cog(x).get_commands():
-                    if not y.hidden:
-                        temp.append(f"\n{y.brief}")
-                embed.add_field(name=f'**{x} :**', value=f"{''.join(temp[0:len(temp)])}", inline=False)
-                temp = []
-            await ctx.send(embed=embed)
-        elif len(c) == 1:
-            c = ''.join(c[:])
-            if self.bot.get_cog(c):
-                await ctx.channel.purge(limit=1)
-                embed = discord.Embed(color=discord.Color.blue(), title=f'"{c} category command list:"')
-                for x in self.bot.get_cog(c).get_commands():
-                    if not x.hidden:
-                        embed.add_field(name=f'**{x.brief} :**', value=f"{x.description}", inline=False)
-                await ctx.send(embed=embed)
-            else:
-                await ctx.send("❌ Category name is invalid!")
-        else:
-            await ctx.send("❌ Input must contain only one argument!")
+            for cmd in self.bot.get_cog(cog).get_commands():
+                if not cmd.hidden:
+                    temp.append(f"{cmd.brief}: {cmd.description}\n")
+            embed.add_field(name=f'**{cog} :**', value=f"{''.join(temp)}", inline=False)
+        await ctx.send(embed=embed)
 
                                 
 def setup(bot):
