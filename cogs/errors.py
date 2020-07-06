7d6e8df4daf3f42317ad27c679381e868f970374
MrSpaar <62711036+MrSpaar@users.noreply.github.com>
2020-07-06 22:23:32 +0200

    Error management system

---
+++ cogs/errors.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class ErrorManager(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        msg = error; msg[0].upper
+        embed = discord.Embed(title="**❌ Une erreur s'est produite :**", description=msg, color=discord.Color.red())
+        await ctx.channel.purge(limit=1)
+        await ctx.send(embed=embed, delete_after=10.0)
+
+def setup(bot):
+    bot.add_cog(ErrorManager(bot))
