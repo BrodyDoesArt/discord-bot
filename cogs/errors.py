28b70a8ecd7ad0a2e8bf18af9f9dcfff854cbf74
MrSpaar <62711036+MrSpaar@users.noreply.github.com>
2020-07-06 23:50:53 +0200

    Error management system
    More understandable error messages

--- cogs/errors.py
+++ cogs/errors.py
@@ -7,10 +7,21 @@ class ErrorManager(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        msg = str(error); msg[0].upper
-        embed = discord.Embed(title="❌ Oops", description=msg, color=discord.Color.red())
+        if isinstance(error, commands.BadArgument):
+            msg = 'You must input an integer!'
+        elif isinstance(error, commands.MissingRequiredArgument):
+            msg = f'You must input these following arguments: {error.param.name}'
+        elif isinstance(error, commands.CommandNotFound):
+            msg = 'Unknown command!'
+        elif isinstance(error, commands.CommandInvokeError):
+            if 'index' in str(error):
+                msg = "Argument is too big!"
+            elif 'NoneType' in str(error):
+                msg = "I'm not connected to any channel!" if 'is_playing' in str(error) else "You're not connected to any channel!"
+
+        embed = discord.Embed(title="❌ Oops :", description=msg, color=discord.Color.red())
         await ctx.channel.purge(limit=1)
-        await ctx.send(embed=embed, delete_after=10.0)
+        await ctx.send(embed=embed, delete_after=5.0)
 
 def setup(bot):
     bot.add_cog(ErrorManager(bot))
